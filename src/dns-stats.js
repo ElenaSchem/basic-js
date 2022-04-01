cc694c3ebc6e64d0c4289a21c6a1c9f402cbf4b6
ElenaSchem <elena.shch91@gmail.com>
2022-04-01 19:39:13 +0300

    feat: add dns-stats solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  domains.forEach(domain => {
+    let revDomain = domain.split('.').reverse();
+    let currentDomain = '';
+    revDomain.forEach(domain => {
+      currentDomain = `${currentDomain}.${domain}`;
+      result[currentDomain] = result[currentDomain] ? result[currentDomain] + 1 : 1;
+    });
+  });
+  return result;
 }
 
 module.exports = {
