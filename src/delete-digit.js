35ea71057d941bb2d1c8d3e5f48e33d00cd27e8e
ElenaSchem <elena.shch91@gmail.com>
2022-04-03 14:27:32 +0300

    feat: add delete-digit solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let max = 0;
+
+  n.toString().split('').forEach((item, i, arr) => {
+    let digitArr = [...arr];
+    digitArr.splice(i, 1);
+    let digit = Number(digitArr.join(''));
+    if (digit > max) max = digit;
+  });
+  
+  return max;
 }
 
 module.exports = {
